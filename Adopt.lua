98fdd5b80dd9de84366a22d3e859ec1b393b9a88
BxngR1ps <cobcorn62@gmail.com>
2024-10-19 01:25:44 -0400

    Adopt.lua

---
+++ Adopt.lua
@@ -0,0 +1,66 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Adopt Me! - 10.19.24 - Kailey <3", "BloodTheme")
+---\\VARS
+PumpknFarm=false
+BasketFarm=false
+TileSkipFarm=false
+---------------\\MAIN TAB
+local Tab = Window:NewTab("Main")
+local MainSection = Tab:NewSection("Main Section")
+MainSection:NewSlider("WalkSpeed", "Walkspeed slider", 500, 16, function(s) -- 500 (MaxValue) | 0 (MinValue)
+	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
+MainSection:NewSlider("JumpPower", "Jumppower slider", 500, 50, function(s) -- 500 (MaxValue) | 0 (MinValue)
+	game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
+end)
+-----------\\AUTO-FARM TAB
+local Tab = Window:NewTab("Auto-Farm")
+local FarmSection = Tab:NewSection("Auto-Farm Section")
+FarmSection:NewToggle("Pumpkin Collect", "Tp To Pumpkins", function(state)
+	if state then
+		PumpknFarm = true
+		if PumpknFarm == true  then
+			for i,v in pairs(workspace.Collectables:GetChildren()) do
+				game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.Collider.CFrame
+				wait(0.4)
+			end
+		end
+	else
+		PumpknFarm = false
+	end
+end)
+
+FarmSection:NewToggle("Candy Basket Collect", "Tp To Pumpkins", function(state)
+	if state then
+		BasketFarm = true
+		if BasketFarm == true  then
+			for i,v in pairs(workspace.HouseExteriors:GetDescendants()) do
+				if v.Name == "CandyBowl" then
+					if v.Candy.polySurface157.Transparency ~= 1 then
+						game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.UseBlock.CFrame
+						wait(1)
+					end
+				end
+			end
+		end
+	else
+		BasketFarm = false
+	end
+end)
+
+FarmSection:NewToggle("TileSkip Farm", "3k Per 10mins", function(state)
+	if state then
+		TileSkipFarm = true
+		if TileSkipFarm == true  then
+			while true do wait()
+				if workspace.Interiors.TileSkipMinigame.Minigame.GoalPlatform:FindFirstChild("GoalCollider") then 
+					game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.Interiors.TileSkipMinigame.Minigame.GoalPlatform.GoalCollider.CFrame
+				end
+				
+			end
+		end
+	else
+		TileSkipFarm = false
+	end
+end)
+
