ee34b846e4a64ce33293ae34e81d430668775750
BxngR1ps <cobcorn62@gmail.com>
2024-10-19 14:55:33 -0400

    Update Adopt.lua

--- Adopt.lua
+++ Adopt.lua
@@ -12,8 +12,15 @@ s:Play()
 PumpknFarm=false
 BasketFarm=false
 TileSkipFarm=false
+Petfarm=false
 Locos={}
+Petz = {}
 
+pet = ""
+shower = ""
+watr = ""
+food = ""
+crib = ""
 ---------------\\MAIN TAB
 local MainTab = Window:NewTab("Main")
 local MainSection = MainTab:NewSection("Main Section")
@@ -36,7 +43,14 @@ TPSection:NewButton("Tp To Nebrhood", "Simple Tp", function()
 	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.Interiors["MainMap!Fall"].Doors["Neighborhood/MainDoor"].WorkingParts.TouchToEnter.CFrame
 end)
 TPSection:NewButton("Tp To Home", "Simple Tp", function()
-	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.Interiors["Neighborhood!Fall"].Doors.MainDoor.WorkingParts.TouchToEnter.CFrame
+
+	for i,v in pairs(workspace.HouseExteriors:GetDescendants()) do
+		if v.Name == "house_owner" then
+			if v.Value == game.Players.LocalPlayer.Name then
+				game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.Parent.Parent.TouchToEnter.CFrame
+			end
+		end
+	end
 end)
 
 local TPSECX = TPSection:NewDropdown("Tp To?","Locations", Locos, function(currentOption)
@@ -90,7 +104,7 @@ FarmSection:NewToggle("Candy Basket Collect", "Tp To Pumpkins", function(state)
 	end
 end)
 
-FarmSection:NewToggle("TileSkip Farm", "3k Per 10mins", function(state)
+FarmSection:NewToggle("TileSkip Auto-Farm", "3k Per 10mins", function(state)
 	if state then
 		TileSkipFarm = true
 		if TileSkipFarm == true  then
@@ -107,8 +121,114 @@ FarmSection:NewToggle("TileSkip Farm", "3k Per 10mins", function(state)
 end)
 
 
+FarmSection:NewButton("House Data-Grab", "Grabs House Data", function()
+	for i,v in pairs(workspace:GetDescendants()) do
+		if v.Name == "ModernShower" then
+			shower = string.sub(v.Parent.Name,22,10000)
+		elseif v.Name == "PetWaterBowl" then
+			watr = string.sub(v.Parent.Name,22,10000)
+		elseif v.Name == "PetFoodBowl" then
+			food = string.sub(v.Parent.Name,22,10000)
+		elseif v.Name == "BasicCrib" then
+			crib = string.sub(v.Parent.Name,22,10000)
+		end
+	end
+end)
 
 
+local SelPet = FarmSection:NewDropdown("Select Pet","Select Your Pet To Auto-Farm With", Petz, function(currentOption)
+	pet = currentOption
+end)
+FarmSection:NewButton("Click Me First <3", "Grabs Tp Locations", function()
+	table.clear(Petz)
+	for i,v in pairs(workspace.Pets:GetChildren()) do
+		table.insert(Petz,v.Name)
+	end
+	SelPet:Refresh(Petz)
+end)
+
+FarmSection:NewToggle("Pet Auto-Farm", "Simple Auto Farm", function(state)
+	if state then
+		Petfarm = true
+		if Petfarm == true  then
+			while true do wait(0.1)
+				game:GetService("Players").LocalPlayer.PlayerGui.AilmentsMonitorApp:WaitForChild("Ailments")
+				for i,v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.AilmentsMonitorApp.Ailments:GetChildren()) do
+					if v:IsA("Frame") then
+						if v.Name == "sleepy" then
+							local args = {
+								[1] = game:GetService("Players").LocalPlayer,
+								[2] = crib,
+								[3] = "UseBlock",
+								[4] = {
+									["cframe"] = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+								},
+								[5] = workspace:WaitForChild("Pets"):WaitForChild(pet)
+							}
+
+							game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("JryxouoJZTLNqHyLsLyaPNKGSUSG"):InvokeServer(unpack(args))
+						elseif v.Name == "dirty" then
+							local args = {
+								[1] = game:GetService("Players").LocalPlayer,
+								[2] = shower,
+								[3] = "UseBlock",
+								[4] = {
+									["cframe"] = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+								},
+								[5] = workspace:WaitForChild("Pets"):WaitForChild(pet)
+							}
+
+							game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("JryxouoJZTLNqHyLsLyaPNKGSUSG"):InvokeServer(unpack(args))
+						elseif v.Name == "hungry" then
+							local args = {
+								[1] = game:GetService("Players").LocalPlayer,
+								[2] = food,
+								[3] = "UseBlock",
+								[4] = {
+									["cframe"] = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+								},
+								[5] = workspace:WaitForChild("Pets"):WaitForChild(pet)
+							}
+
+							game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("JryxouoJZTLNqHyLsLyaPNKGSUSG"):InvokeServer(unpack(args))
+						elseif v.Name == "bored" then
+
+						elseif v.Name == "thirsty" then
+							local args = {
+								[1] = game:GetService("Players").LocalPlayer,
+								[2] = watr,
+								[3] = "UseBlock",
+								[4] = {
+									["cframe"] = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+								},
+								[5] = workspace:WaitForChild("Pets"):WaitForChild(pet)
+							}
+
+							game:GetService("ReplicatedStorage"):WaitForChild("API"):WaitForChild("JryxouoJZTLNqHyLsLyaPNKGSUSG"):InvokeServer(unpack(args))
+						elseif v.Name == "sick" then
+
+						elseif v.Name == "school" then
+
+						elseif v.Name == "salon" then
+
+						elseif v.Name == "pizza_party" then
+
+						elseif v.Name == "camping" then
+
+						elseif v.Name == "beach_party" then
+
+
+						end		
+
+
+					end
+				end
+			end
+		end
+	else
+		Petfarm = false
+	end
+end)
 ---------------\\Settings TAB
 local Tab = Window:NewTab("Settings")
 local SettingsSection = Tab:NewSection("Settings Section")
@@ -136,3 +256,5 @@ end)
 MiscSection:NewButton("Dark Dex", "Simple Admin", function()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/infyiff/backup/main/dex.lua"))()
 end)
+
+
